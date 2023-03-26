378b1352822ef9caa6e651b2b02c0fe09c1c48d2
Subhasree <subhasree4921@gmail.com>
2023-03-26 17:49:47 +0530

    first commit

--- main/forms.py
+++ main/forms.py
@@ -1,18 +1,31 @@
 from django.forms import ModelForm
-from .models import Answer,Question,CustomUser
+from django.contrib.auth.forms import UserCreationForm, UsernameField
+from .models import Answer, Question, CustomUser
 from django.contrib.auth import get_user_model
+
 User = get_user_model()
+
+
 class AnswerForm(ModelForm):
     class Meta:
-        model=Answer
-        fields=('detail',)
+        model = Answer
+        fields = ("detail",)
+
 
 class QuestionForm(ModelForm):
     class Meta:
-        model=Question
-        fields=('title','detail','tags')
+        model = Question
+        fields = ("title", "detail", "tags")
+
 
 class ProfileForm(ModelForm):
     class Meta:
-        model=CustomUser
-        fields=('first_name','last_name','username','bio','location')
+        model = CustomUser
+        fields = ("first_name", "last_name", "username", "bio", "location")
+
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta:
+        model = CustomUser
+        fields = ("username",)
+        field_classes = {"username": UsernameField}
